a4acc87893c4cc6b23b9df6d1401ea080f2cae67
AbdullahELkadyA <abdallahatem98@gmail.com>
2024-05-19 16:47:55 +0300

    upload project

---
+++ src/main/java/appium/demo/BasePage.java
@@ -0,0 +1,44 @@
+package appium.demo;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
+
+import io.appium.java_client.android.AndroidDriver;
+
+public class BasePage {
+	public AndroidDriver driver ;
+    public JavascriptExecutor jse ;
+    public Select select ;
+    public Actions action ;
+
+    // create constructor
+    public BasePage(AndroidDriver driver)
+    {
+    	 this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    protected static void clickButton(WebElement button)
+    {
+        button.click();
+    }
+
+    protected static void setTextElementText(WebElement textElement , String value)
+    {
+        textElement.sendKeys(value);
+    }
+    public void scrollToBottom()
+
+    {
+        jse.executeScript("scrollBy(0,2500)");
+    }
+
+    public void clearText(WebElement element)
+    {
+        element.clear();
+    }
+}
